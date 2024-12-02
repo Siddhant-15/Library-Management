efbacda864b9d94e8d351db2db4f353ca0f43c33
Siddhant Janbandhu <63910007+Siddhant-15@users.noreply.github.com>
2024-12-02 15:53:00 +0530

    Update ShowBook.jsx

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -11,7 +11,7 @@ const ShowBook = () => {
 
     useEffect(() => {
         setLoading(true);
-        axios.get(`https://library-management-api-phi.vercel.app/?vercelToolbarCode=EHxMTG6ZZ8_r_Uz/${id}`)
+        axios.get(`https://library-management-api-phi.vercel.app/?vercelToolbarCode=EHxMTG6ZZ8_r_Uz/books/${id}`)
             .then((response) => {
                 setBook(response.data);
                 setLoading(false);
