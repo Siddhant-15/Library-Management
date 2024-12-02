9fff3e5b08d6b50f6b6c4d8b23e4817f5eebcdc9
Sikha <sikhak390@gmail.com>
2024-12-02 11:59:16 +0530

    first

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://siddhantjanbandhu18:LiGJ96tyGApJt8tO@cluster0.2qdy1.mongodb.net/books?retryWrites=true&w=majority&appName=Cluster0'
---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import express, { request } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from './models/bookModel.js';
+import bookRoute from './routes/booksroutes.js';
+import cors from 'cors';
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+//Middleware for handling CORS POLICY
+//option 1: Allow All Origins with Default cors(*)
+app.use(cors(
+    {
+        origin: [],
+        methods: ["POST", "GET", "PUT", "DELETE"],
+        credentials: true
+    }
+));
+
+//Option 2: Allow Custom Origins
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['Get', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome To MERN Stack')
+});
+
+app.use('/books', bookRoute);
+
+
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is Listening to port: ${PORT}`);
+        });
+    }).catch((error) => {
+        console.log(error);
+    });
