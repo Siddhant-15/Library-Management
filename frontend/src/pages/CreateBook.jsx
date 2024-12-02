50c069fa79912dba941b52bef4c7c92d8abeeb7b
Siddhant Janbandhu <63910007+Siddhant-15@users.noreply.github.com>
2024-12-02 12:08:06 +0530

    Update CreateBook.jsx

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
         };
         setLoading(true);
         axios
-            .post('http://localhost:5555/books', data)
+            .post('https://library-management-api-phi.vercel.app/?vercelToolbarCode=EHxMTG6ZZ8_r_Uz/books', data)
             .then(() => {
                 setLoading(false);
                 enqueueSnackbar('Book Created successfully', { variant: 'success' });
