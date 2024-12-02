1c3e72bda6a68e7e8b41fd9fca1ead3e297eb5fe
Siddhant Janbandhu <63910007+Siddhant-15@users.noreply.github.com>
2024-12-02 12:09:46 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -16,7 +16,7 @@ const EditBook = () => {
 
     useEffect(() => {
         setLoading(true);
-        axios.get(`http://localhost:5555/books/${id}`)
+        axios.get(`https://library-management-api-phi.vercel.app/?vercelToolbarCode=EHxMTG6ZZ8_r_Uz/books/${id}`)
             .then((response) => {
                 setAuthor(response.data.author);
                 setPublishYear(response.data.publishYear)
